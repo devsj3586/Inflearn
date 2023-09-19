1f5274cf9bd9df2ed7d5c3ad38c17e10d121ab16
charcoal <devsj3586@gmail.com>
2023-09-19 18:00:35 +0900

    Inflearn Spring Study - FixDiscount -> RateDiscount 로 변경

--- Spring/src/main/java/spring/core/AppConfig.java
+++ Spring/src/main/java/spring/core/AppConfig.java
@@ -2,6 +2,7 @@ package spring.core;
 
 import spring.core.discount.DiscountPolicy;
 import spring.core.discount.FixDiscountPolicy;
+import spring.core.discount.RateDiscountPolicy;
 import spring.core.member.MemberRepository;
 import spring.core.member.MemberService;
 import spring.core.member.MemberServiceImpl;
@@ -27,6 +28,7 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
