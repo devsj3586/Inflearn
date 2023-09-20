b5bb5cd6da99493804a712e78314fc0304a56f0c
charcoal <devsj3586@gmail.com>
2023-09-20 17:48:06 +0900

    Inflearn Spring Study - @Configuration , @Bean 등록

--- Spring/src/main/java/spring/core/AppConfig.java
+++ Spring/src/main/java/spring/core/AppConfig.java
@@ -1,7 +1,8 @@
 package spring.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import spring.core.discount.DiscountPolicy;
-import spring.core.discount.FixDiscountPolicy;
 import spring.core.discount.RateDiscountPolicy;
 import spring.core.member.MemberRepository;
 import spring.core.member.MemberService;
@@ -10,23 +11,28 @@ import spring.core.member.MemoryMemberRepository;
 import spring.core.order.OrderService;
 import spring.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
     /**
      * 실제 동작에 필요한 구현 객체를 생성
      * AppConfig는 생성한 객체 인스턴스의 참조를 생성자를 통해서 주입해준다
      */
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(MemberRepository());
     }
 
-    private MemberRepository MemberRepository() {
+    @Bean
+    public MemberRepository MemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
