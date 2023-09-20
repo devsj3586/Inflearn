571a717285d8b6b0c57a95ff148f384634027993
charcoal <devsj3586@gmail.com>
2023-09-20 17:48:52 +0900

    Inflearn Spring Study - 스프링 컨테이너를 통해 DI

--- Spring/src/main/java/spring/core/MemberApp.java
+++ Spring/src/main/java/spring/core/MemberApp.java
@@ -1,5 +1,7 @@
 package spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 import spring.core.member.MemberService;
@@ -7,8 +9,17 @@ import spring.core.member.MemberService;
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        /**
+         * 자바 코드로만  DI
+         */
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        /**
+         * 스프링 컨테이너 적용
+         */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
