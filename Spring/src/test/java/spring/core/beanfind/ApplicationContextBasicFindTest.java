fb08ae63429b6bc6813810a15800b71a7be50af4
charcoal <devsj3586@gmail.com>
2023-09-22 17:39:48 +0900

    Inflearn Spring Study - 스프링 빈 조회 - 기본

---
+++ Spring/src/test/java/spring/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,56 @@
+package spring.core.beanfind;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.core.AppConfig;
+import spring.core.member.MemberService;
+import spring.core.member.MemberServiceImpl;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름 으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        //검증
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean( MemberService.class);
+        //검증
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberServiceImpl memberService = ac.getBean( MemberServiceImpl.class);
+        //검증
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    /**
+     * 실패했을때 하는 Test
+     */
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+//        ac.getBean("xxxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class));
+
+    }
+}
+
+
