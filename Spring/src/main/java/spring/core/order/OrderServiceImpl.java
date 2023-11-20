c56adc1ca0e34e2fac8e9d21248331c9c1f3913f
charcoal <devsj3586@gmail.com>
2023-11-20 16:18:18 +0900

    Inflearn Spring Study - 빈 2개 이상 충돌시 @Autowired,@Qualifier,@Primary 사용방법

--- Spring/src/main/java/spring/core/order/OrderServiceImpl.java
+++ Spring/src/main/java/spring/core/order/OrderServiceImpl.java
@@ -2,23 +2,24 @@ package spring.core.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import spring.core.discount.DiscountPolicy;
 import spring.core.member.Member;
 import spring.core.member.MemberRepository;
 
 @Component
-@RequiredArgsConstructor // final이 붙은 필드를 모아서 생성자를 생성
+//@RequiredArgsConstructor // final이 붙은 필드를 모아서 생성자를 생성 lombok
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired  // 생성자가 한개일때 생략 가능
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired  // 생성자가 한개일때 생략 가능 (!!)
+    public OrderServiceImpl(MemberRepository memberRepository,DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
